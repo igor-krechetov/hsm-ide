15d86a2b23d6f167279fff318310571dfb856be8
igor-krechetov <58586005+igor-krechetov@users.noreply.github.com>
2025-07-27 14:46:17 +0100

    [refactoring] delete reference py files

--- state_ide/hsm_ide.cmd
+++
@@ -1,1 +0,0 @@
-start /b pythonw .\hsm_ide.py
--- state_ide/make_dist.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pyi-makespec --onefile --hidden-import PySide6.QtXml --add-data 'ui/main.ui':'ui' --add-data 'ui/settings.ui':'ui' ./hsm_ide.py
-pyinstaller ./hsm_ide.spec
